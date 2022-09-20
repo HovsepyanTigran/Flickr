c2a77ae044c883bbb67346b49819a042071cc38d
Tigran Hovsepyan <87910637+HovsepyanTigran@users.noreply.github.com>
2022-09-20 20:47:58 +0400

    Flickr API error

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 window.onload = function(){
 
-  document.querySelector('.flickr-wrapper__button-start').addEventListener('click',function(){
-    
+
+
+  document.querySelector('.flickr-wrapper__button-start').onclick = function() {
+    document.querySelector('.flickr-wrapper__button-start').style.visibility = 'hidden';
+    }    
     new Flickr({
       container: document.querySelector('.flickr-container')
   })
 
-  })
+  
 }
-
+var key = '979b6782aab93b8ca2f3fce0f80b652d'
+let url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&tags=car&per_page=10&page=1&format=json&nojsoncallback=1'
+fetch(url).then(function(resp) {
+return resp.json()
+}).then(function(j) {
+  return j.photos.photo.map((pic) => {
+    console.log(pic);
+  let srcPath = 'hhtps://farm'+pic.farm+'.statickFlickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+  let img = document.createElement('img');
+            img.classList.add("img");
+            document.querySelector('.flickr-container').append(img);
+  img.src = srcPath
+  
+  
+  }
+  )
+  console.log(j)
+})
--- main.js
+++ main.js
@@ -7,19 +7,28 @@ class Flickr {
     }
     drawInput() {
         document.querySelector('.flickr-wrapper__button-start').onclick = function() {
-        document.querySelector('.flickr-wrapper__button-start').style.visibility = 'hidden';
-        }
-        var flickrSearcher = document.createElement('div');  
-        flickrSearcher.classList.add("flickr-searcher");
-        document.querySelector('.flickr-container').append(flickrSearcher);
+            document.querySelector('.flickr-wrapper__button-start').style.visibility = 'hidden';
+                
+            var flickrSearcher = document.createElement('div');  
+            flickrSearcher.classList.add("flickr-searcher");
+            document.querySelector('.flickr-container').append(flickrSearcher);
 
-        var flickrInput = document.createElement('input');
-        flickrInput.classList.add("flickr-input");
-        document.querySelector('.flickr-searcher').append(flickrInput);
-    
-        var flickrSubmit = document.createElement('input');
-        flickrSubmit.classList.add("flickr-submit");
-        flickrSubmit.type = "submit";
-        document.querySelector('.flickr-searcher').append(flickrSubmit);
+            var flickrInput = document.createElement('input');
+            flickrInput.classList.add("flickr-input");
+            document.querySelector('.flickr-searcher').append(flickrInput);
+        
+            var flickrSubmit = document.createElement('input');
+            flickrSubmit.classList.add("flickr-submit");
+            flickrSubmit.type = "submit";
+            document.querySelector('.flickr-searcher').append(flickrSubmit);
+            var strArr;
+            flickrSubmit.onclick = (function() {
+                strArr = flickrInput.value.split(/[,.\s]/).filter(function(item) {
+                return item.length != 0});
+                console.log(strArr);
+            })
+        }
     }
 }
+
+

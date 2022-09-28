5e738851918ffa9659b738cafd85c970770b2ece
Tigran Hovsepyan <87910637+HovsepyanTigran@users.noreply.github.com>
2022-09-28 18:51:28 +0400

    drag & drop received pictures in the appropriate basket

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ class Flickr {
       options.api_token || "979b6782aab93b8ca2f3fce0f80b652d";
     this.options.per_page = options.images_limit || 5;
     this.options.api_url = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
-    this.images = [];
     this.flickrImgsImg;
     this.flickrBoxesBox;
     if (this.container) this.render();
@@ -25,34 +24,37 @@ class Flickr {
     flickrSubmit.type = "submit";
     flickrSearcher.append(flickrSubmit);
     var strArr;
+
+    var submitClick = true;
     flickrSubmit.onclick = () => {
-      strArr = flickrInput.value.split(/[,.\s]/).filter(function (item) {
-        return item.length != 0;
-      });
-      let flickrImgs = document.createElement("div");
-      flickrImgs.classList.add("flickr-imgs");
-      this.container.append(flickrImgs);
+      if(submitClick === true) {
+        strArr = flickrInput.value.split(/[,.\s]/).filter(function (item) {
+          return item.length != 0;
+        });
+        let flickrImgs = document.createElement("div");
+        flickrImgs.classList.add("flickr-imgs");
+        this.container.append(flickrImgs);
 
-      strArr.map(async (item) => {
-        let url =
-          this.options.api_url +
-          "&api_key=" +
-          this.options.api_token +
-          "&tags=" +
-          item +
-          "&per_page=" +
-          this.options.per_page +
-          "&page=3&format=json&nojsoncallback=1";
-        let result = await fetch(url);
+        strArr.map(async (item) => {
+          let url =
+            this.options.api_url +
+            "&api_key=" +
+            this.options.api_token +
+            "&tags=" +
+            item +
+            "&per_page=" +
+            this.options.per_page +
+            "&page=3&format=json&nojsoncallback=1";
+          let result = await fetch(url);
+          try{
+              result = await result.json();
+          }catch(ex){
+              console.log(ex)
+          }
+          
+          let photos = result.photos.photo;
 
-        try{
-            result = await result.json();
-        }catch(ex){
-            console.log(ex)
-        }
-        
-        let photos = result.photos.photo;
-
+          
         photos.map((pic) => {
             let srcPath =
               "https://farm" +
@@ -67,33 +69,35 @@ class Flickr {
 
             this.flickrImgsImg = document.createElement("img");
             this.flickrImgsImg.classList.add("flickr-imgs__img");
-            for(let i = 0; i < strArr.length; i++) {
-              this.flickrImgsImg.id = strArr[i]
-            }
+            this.flickrImgsImg.alt = pic.name;
             flickrImgs.append(this.flickrImgsImg);
             this.flickrImgsImg.src = srcPath;
             
-
-            this.images.push({srcPath})
-            this.images.push(this.flickrImgsImg);
-            function shuffleArray(array) {
-              for (var i = array.length - 1; i > 0; i--) {
-                var j = Math.floor(Math.random() * (i + 1));
-                var temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-              }
-              return array;
-            }
-            shuffleArray(this.images);
-      
-          });
-          let dragged = null;
+           
+        });
+        // for(let i = 0;i < photos.length;i++) {
+        //   photos[i].name = item;
+          
+        //   function shuffleArray(array) {
+        //     for (var i = array.length - 1; i > 0; i--) {
+        //       var j = Math.floor(Math.random() * (i + 1));
+        //       var temp = array[i];
+        //       array[i] = array[j];
+        //       array[j] = temp;
+        //     }
+        //     return array;
+        //   }
+        //   shuffleArray(photos);
+          
+        // }
+      let dragged = null;
       this.flickrBoxesBox = document.querySelectorAll(".flickr-boxes__box")
       this.flickrImgsImg = document.querySelectorAll(".flickr-imgs__img")
       this.flickrImgsImg.forEach((item) => {item.addEventListener("dragstart", (event) => {
         dragged = event.target;})
       });
+      
+
       this.flickrBoxesBox.forEach(function(item) {
         item.addEventListener("dragover", (event) => {
           event.preventDefault();
@@ -102,22 +106,13 @@ class Flickr {
       this.flickrBoxesBox.forEach((item) => {
         item.addEventListener("drop", (event) => {
         event.preventDefault();
-        if (event.target.textContent === 'dog') {
+        if (event.target.textContent === dragged.alt) {
           // dragged.parentNode.removeChild(dragged);
           event.target.appendChild(dragged);
           dragged.style.visibility = 'hidden';
-          // console.log(event.target.textContent);
         }
-        console.log(this.flickrBoxesBox);
-        this.flickrBoxesBox.onclick = () => {
-          let showBoxContent = document.createElement("div");
-          showBoxContent.classList.add("flickr-boxes-content");
-          this.container.append(showBoxContent);
-
-
-         }
-      })});
-      
+        });
+      });
       });
   
       let flickrBoxes = document.createElement("div");
@@ -129,6 +124,11 @@ class Flickr {
         this.flickrBoxesBox.classList.add("flickr-boxes__box");
         document.querySelector(".flickr-boxes").append(this.flickrBoxesBox);
       }
+      this.flickrBoxesBox.onclick = () => {
+        let showBoxContent = document.createElement("div");
+        showBoxContent.classList.add("flickr-boxes-content");
+        this.container.append(showBoxContent);
+      }
       
       
       
@@ -136,6 +136,8 @@ class Flickr {
       for (let i = 0; i < strArr.length; i++) {
         this.container.querySelectorAll(".flickr-boxes__box")[i].textContent = strArr[i];
       }
-    };
+      submitClick = false;
+    }
+  }
   }
 }
